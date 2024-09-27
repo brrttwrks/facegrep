1064462563c78df6425d75b0e3c5e0bf1da1ae92
Eric Nicholas Barrett <eric@occrp.org>
2024-09-27 18:59:05 +0200

    update model using orm

--- facegrep/model.py
+++ facegrep/model.py
@@ -1,18 +1,80 @@
-#from .settings import FACEGREP_POSTGRES_URI
+from __future__ import annotations
+
+from settings import FACEGREP_POSTGRES_URI
+from sqlalchemy import create_engine
 from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.orm import Mapped
+from sqlalchemy.orm import mapped_column
+from sqlalchemy.orm import relationship
+from sqlalchemy import Column
+from sqlalchemy import Table
+from sqlalchemy import ForeignKey
+from sqlalchemy.types import Text
+from pgvector.sqlalchemy import Vector
 
 
-class DeclarativeBase():
+class Base(DeclarativeBase):
     pass
+
+
+file_tag_assoc_table = Table(
+    "files_tags",
+    Base.metadata,
+    Column(
+        "file_id",
+        ForeignKey("files.id"),
+        primary_key=True
+    ),
+    Column(
+        "filetag_id",
+        ForeignKey("filetags.id"),
+        primary_key=True
+    ),
+)
 
 
 class File(Base):
-    pass
+    __tablename__ = "files"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    hash = Column("hash", Text)
+    tags: Mapped[list["FileTag"]] = relationship(
+        secondary=file_tag_assoc_table,
+        back_populates = "files",
+    )
+    embeddings: Mapped[list["Embedding"]] = relationship()
+
+
+class FileTag(Base):
+    __tablename__ = "filetags"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    source =  Column("source", Text)
+    files: Mapped[list[File]] = relationship(
+        secondary=file_tag_assoc_table,
+        back_populates = "filetags",
+    )
 
 
 class Embedding(Base):
-    pass
+    __tablename__ = "embeddings"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    embedding = Column(Vector(4096))
+    file_id: Mapped[int] = mapped_column(ForeignKey("files.id"))
+    file: Mapped["File"] = relationship(back_populates="embeddings")
+    person_id: Mapped[int] = mapped_column(ForeignKey("persons.id"))
+    person: Mapped["Person"] = relationship(back_populates="embeddings")
 
 
 class Person(Base):
-    pass
+    __tablename__ = "persons"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name = Column("name", Text)
+    embeddings: Mapped[list["Embedding"]] = relationship()
+
+
+if __name__ == "__main__":
+    engine = create_engine(FACEGREP_POSTGRES_URI, echo=True)
+    Base.metadata.create_all(engine)
